9d37cee9f1bd2f7e248d2b470986366f6747dd40
Yenno Van Rampelbergh <yenno.vanrampelbergh@student.romerocollege.be>
2020-11-19 17:38:38 +0100

    Maal

---
+++ MaalTafels/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaalTafels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//declarations
+            byte product, getal;
+            string tafellijn;
+
+            //INPUT
+            //Vraag een getal
+            Console.WriteLine();
+            Console.Write("Geef een getal in tussen 1 en 10 ");
+            Console.ReadLine();
+
+
+            //processing + output
+            // HERHAAL factor VAN TOT EN MET 10 STAPGROOTTE 1
+            for (int factor = 1; factor <= 10; factor++)
+            {
+                // Bereken product( = factor x gegeven getal)
+                product = (byte)(factor * (byte)getal);
+
+                // Bereken tafelLijn("{factor} x 3 = {product}")
+                tafellijn = $"{factor} * getal = {product}";
+
+                // Toon tafelLijn
+                Console.WriteLine();
+            }
+
+
+            // Wachten
+            Console.Write("Druk op Enter om af te sluiten ...");
+            Console.ReadLine();
+        }
+    }
+}
